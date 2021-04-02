c45ab3dbb958b8d16f5dd846fb12e99b384e1b8e
Sean McMenamin <16652523+mimanim@users.noreply.github.com>
2021-04-02 09:49:38 -0600

    DeckGL setup
    Co-authored-by: mccheffin <mccheffin@users.noreply.github.com>

---
+++ src/components/MapContainer.tsx
@@ -0,0 +1,25 @@
+import DeckGLController from '@deck.gl/react';
+import React from 'react';
+import {StaticMap} from 'react-map-gl';
+
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 13,
+  pitch: 0,
+  bearing: 0
+};
+
+const MapContainer: React.FC = () => {
+  return (
+    <DeckGLController
+      initialViewState={INITIAL_VIEW_STATE}
+      controller
+      layers={[]}
+    >
+      <StaticMap mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN} />
+    </DeckGLController>
+  );
+};
+
+export default MapContainer;
