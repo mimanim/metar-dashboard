f054f34d317fb7283e9fd93a3db0841bac226b40
Chris McCutcheon <cmccutcheon2015@gmail.com>
2021-04-02 15:23:31 -0600

    data section

--- src/components/WeatherStationDetails.tsx
+++ src/components/WeatherStationDetails.tsx
@@ -8,7 +8,7 @@ import { WeatherStation } from "../types";
 
 const StyledWeatherStationCard = styled.div`
   width: 100%;
-  flex: 1;
+  width: 420px;
 `;
 
 const StyledWeatherStationCardContents = styled.div`
@@ -17,9 +17,40 @@ const StyledWeatherStationCardContents = styled.div`
   flex-direction: column;
 `;
 
-const StyledStationName = styled.h4``;
+const StyledCardLine = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  padding: 8px 20px;
+  box-sizing: border-box;
+`;
+
+const StyledWeatherStationTitle = styled(StyledCardLine)`
+  padding: 8px 20px;
+
+`;
+
+const StyledStationName = styled.h4`
+  margin: 0;
+`;
 
 const StyledDataElement = styled.span``;
+
+const StyledStationDataContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 32px;
+  align-items: center;
+`;
+
+const StyledDataTitleWrapper = styled.div`
+  width: 90%;
+  border-bottom: solid 2px #339c3f;
+  display: flex;
+  justify-content: center;
+`;
+
 
 const WeatherStationDetails: React.FC = () => {
   const [selectedObjectId] = React.useContext(SelectionContext);
@@ -35,20 +66,36 @@ const WeatherStationDetails: React.FC = () => {
     name,
     id,
     mindate,
-    maxdate,
-    datacoverage,
+    maxdate
   } = selectedStation;
 
   return (
     <StyledWeatherStationCard key={id}>
       <StyledWeatherStationCardContents>
-        <StyledStationName>{name}</StyledStationName>
-        <StyledDataElement>{elevation}</StyledDataElement>
-        <StyledDataElement>{latitude}</StyledDataElement>
-        <StyledDataElement>{longitude}</StyledDataElement>
-        <StyledDataElement>{mindate}</StyledDataElement>
-        <StyledDataElement>{maxdate}</StyledDataElement>
-        <StyledDataElement>{datacoverage}</StyledDataElement>
+        <StyledWeatherStationTitle>
+          <StyledStationName>{name}</StyledStationName>
+        </StyledWeatherStationTitle>
+        <StyledCardLine>
+          <StyledDataElement>{`Elevation: ${elevation} meters`}</StyledDataElement>
+        </StyledCardLine>
+        <StyledCardLine>
+          <StyledDataElement>{`Lat: ${latitude.toFixed(5)}`}</StyledDataElement>
+          <StyledDataElement>{`Lon: ${longitude.toFixed(
+          5
+        )}`}</StyledDataElement>
+        </StyledCardLine>
+        <StyledCardLine>
+          <StyledDataElement>{`Online: ${mindate ? mindate : '-'}`}</StyledDataElement>
+          <StyledDataElement>{`Offline: ${maxdate ? maxdate : '-'}`}</StyledDataElement>
+        </StyledCardLine>
+        <StyledStationDataContainer>
+          <StyledDataTitleWrapper>
+            <StyledStationName>
+              Station Data
+            </StyledStationName>
+          </StyledDataTitleWrapper>
+
+        </StyledStationDataContainer>
       </StyledWeatherStationCardContents>
     </StyledWeatherStationCard>
   );
